ac907708a6652a63acb1766916831e449a972116
Brendon <60971135+brendon-liew@users.noreply.github.com>
2021-03-17 02:36:16 +0000

    Update addProduct.php

--- Website/addProduct.php
+++ Website/addProduct.php
@@ -5,23 +5,51 @@ require_once("config.php");
 
 if(isset($_POST['submit'])){
 	
-	$test = $_POST['fname'];
-	$filename = $_FILES["uploadImg"]["name"]; 
-    $tempname = $_FILES["uploadImg"]["tmp_name"];     
-	//Save image to the folder
-	//Can change the folder name
-    $folder = "Img/".$filename; 
-    $test2 = $_POST['genders'];  
-	$test3 = $_POST['cat'];  	
-	$test4 = $_POST['qua'];  	
+	
 	//inserting image details (ie image name) in the database
-	 $sql = "INSERT INTO product (pname,pimg,gender,category,quantity) VALUES ('$test','$filename','$test2','$test3','$test4')";
-	$conn->exec($sql);	
-            if (move_uploaded_file($tempname, $folder))  { 
-            $msg = "Image uploaded successfully"; 
-        }else{ 
-            $msg = "Failed to upload image"; 
-		}
+	 //Prepare stament
+	 $stmt = $conn->prepare("INSERT INTO product (pname,pimg,gender,category,quantity,price) VALUES(:fname,:uploadImg,:genders,:cat,:qua,:pri)");
+	 
+	 //Bind value 
+	 $stmt->bindParam(':fname', $fname);
+	 $stmt->bindParam(':uploadImg', $filename);
+	 $stmt->bindParam(':genders', $gender);
+	 $stmt->bindParam(':cat', $cate);
+	 $stmt->bindParam(':qua', $qual);
+	 $stmt->bindParam(':pri', $pric);
+	 
+	//Get value from the form 
+	$fname = $_POST['fname'];
+	$filename = $_FILES['uploadImg']['name']; 
+    $tempname = $_FILES['uploadImg']['tmp_name'];     
+	//Create folder in current directory
+	//Can change the folder name
+    $folder = getcwd()."/Img/"; 
+	
+    $gender = $_POST['genders'];  
+	$cate = $_POST['cat'];  	
+	$qual = $_POST['qua'];  	
+	$pric = $_POST['pri'];  
+	
+	//Check see whether this directory exist current folder 
+	if(!is_dir($folder)){
+		mkdir($folder,0755);
+	}
+	//Save image to the folder
+	  $folder1 = getcwd()."/Img/".$filename; 
+	  
+	 if(move_uploaded_file($tempname, $folder1)){
+		echo "Image uploaded successfully"; 
+	 }else{
+		 echo "Fail to upload";
+	 }
+	//Execute the value 
+	$stmt->execute();	
+	
+
+	
+	
+      
 
 // try {
 	
