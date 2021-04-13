62d11ae7fc3008f80a224c84736c76b423a85b47
william <42318592+backendprogrammer@users.noreply.github.com>
2021-04-13 17:01:36 +0100

    Create loginScript.php

---
+++ login and signup/loginScript.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+require_once('config2.php');
+
+
+$email = $_POST['email'];
+$email = trim($email);
+
+
+$password = $_POST['password'];
+$password = trim($password);
+$password = md5($password);
+$error = 0;
+
+if (!isset($_POST["submit"])) {
+    $error = +1;
+    echo "<script>
+window.location.href='login.php';
+</script>";
+}
+
+if ($error === 0) {
+    if (empty($email) || empty($password)) {
+        echo "<script>
+alert('please input both a email and password');
+window.location.href='login.php';
+</script>";
+
+    } else {
+        $sql = "SELECT * FROM `user` WHERE userName='$email' AND '$password'";
+        $query = mysqli_query($con, $sql);
+        $res = mysqli_num_rows($query);
+
+
+        if ($res == 1) {
+            echo "<script>                
+                    window.location.href='index.php';
+                    </script>";
+
+            $_SESSION['logged-in'] = true;
+            $_SESSION['username'] = $username;
+        } else {
+            echo "Invalid Username or Password";
+        }
+    }
+}
+?>
