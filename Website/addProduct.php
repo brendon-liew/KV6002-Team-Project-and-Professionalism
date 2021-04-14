e58114364c7da74179acacceb02d5601b3e5668d
Brendon <60971135+brendon-liew@users.noreply.github.com>
2021-04-14 01:35:38 +0100

    Add files via upload

--- Website/addProduct.php
+++ Website/addProduct.php
@@ -2,22 +2,34 @@
 
 require_once("config.php");
 
-
+$cate = isset($_POST['cat'])? $_POST['cat'] : null;
 if(isset($_POST['submit'])){
 	try {
 	
 	//inserting image details (ie image name) in the database
+	
+	
 	 //Prepare stament
-	 $stmt = $conn->prepare("INSERT INTO product (pname,pimg,gender,category,quantity,price) VALUES(:fname,:uploadImg,:genders,:cat,:qua,:pri)");
-	 
+	 if($cate==1){
+	 $stmt = $conn->prepare("INSERT INTO jacket (jaName,jaImg,jaDesc,jaPrice,genID) VALUES(:fname,:uploadImg,:info,:pri,:genders)");
+	 }elseif($cate==2){
+	 $stmt = $conn->prepare("INSERT INTO shirt (shrtName,shrtImg,shrtDesc,shrtPrice,genID) VALUES(:fname,:uploadImg,:info,:pri,:genders)");
+	 }elseif($cate==3){
+	 $stmt = $conn->prepare("INSERT INTO shoes (shName,shImg,shDesc,shPrice,genID) VALUES(:fname,:uploadImg,:info,:pri,:genders)");
+	 }elseif($cate==4){
+	 $stmt = $conn->prepare("INSERT INTO pants (paName,paImg,paDesc,paPrice,genID) VALUES(:fname,:uploadImg,:info,:pri,:genders)");
+	 }elseif($cate==5){
+	 $stmt = $conn->prepare("INSERT INTO accessories (asName,asImg,asDesc,asPrice,genID) VALUES(:fname,:uploadImg,:info,:pri,:genders)");
+	 }
 	 //Bind value 
 	 $stmt->bindParam(':fname', $fname);
 	 $stmt->bindParam(':uploadImg', $filename);
 	 $stmt->bindParam(':genders', $gender);
-	 $stmt->bindParam(':cat', $cate);
-	 $stmt->bindParam(':qua', $qual);
+	 //$stmt->bindParam(':cat', $cate);
 	 $stmt->bindParam(':pri', $pric);
-	 
+	 $stmt->bindParam(':info', $des);
+	
+	
 	//Get value from the form 
 	$fname = $_POST['fname'];
 	$filename = $_FILES['uploadImg']['name']; 
@@ -27,14 +39,14 @@ if(isset($_POST['submit'])){
     $folder = getcwd()."/Img/"; 
 	
     $gender = $_POST['genders'];  
-	$cate = $_POST['cat'];  	
-	$qual = $_POST['qua'];  	
+	//$cate = $_POST['cat'];  	
+	 	
 	$pric = $_POST['pri'];  
+	$des = $_POST['info']; 
 	
-	
-	 $stmt1 = $conn->prepare("Select * from product where pname='" . $fname . "'");
-	 $stmt1->execute();
-	 $result = $stmt1->fetchAll();
+	 //$stmt1 = $conn->prepare("Select * from product where pname='" . $fname . "'");
+	// $stmt1->execute();
+	// $result = $stmt1->fetchAll();
 	
 	
 	
@@ -54,15 +66,16 @@ if(isset($_POST['submit'])){
 		 echo "Fail to upload";
 	 }
 	 
+	 $stmt->execute();
 	 //Check whether the value is exist in database
-	 if(count($result)==0){
-			echo "Insert succesfully";
+	 //if(count($result)==0){
+			//echo "Insert succesfully";
 			
 			//Execute the value 
-			$stmt->execute();
-	}else{
-			echo "<br>"."Value exist";
-	}
+			//$stmt->execute();
+	//}else{
+			//echo "<br>"."Value exist";
+	//}
 
 		} catch(PDOException $e) {
 	echo  "<br>" . $e->getMessage();
